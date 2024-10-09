7c3bee397133017621ecd34791cca0891f6da5aa
Sukruthi <sukruthisantosh@gmail.com>
2024-10-09 21:38:39 +0100

    longest repeating substring without repeating char

---
+++ sliding_window/longest_repeating_char_replacement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        return 0
+
+
+solution = Solution()
+s = "ABAB"
+k = 2
+print(solution.characterReplacement(s=s, k=k))
